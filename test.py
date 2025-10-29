fddedeebf365d3a78119fe488ddea26e601b5bd8
dalis44 <morosan_eugen@yahoo.com>
2025-10-29 18:32:11 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,34 @@
+from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
+from functions.write_file import write_file
+
+def test():
+  result = get_files_info(r"D:\AWORK\wrapper\calculator",".")
+  print("The output of root directory is : ")
+  print(result)
+  print("")
+
+  result = get_files_info(r"D:\AWORK\wrapper\calculator","pkg")
+  print("The output of 'pkg' directory is : ")
+  print(result)
+  print("")  
+
+
+  #result = get_files_info(r"D:\AWORK\wrapper\calculator", "../")
+  #print("Result for '../' directory:")
+  #print(result)
+
+  result = get_file_content(r"D:\AWORK\wrapper\calculator", "main.py")
+  print(result)
+
+  result = get_file_content(r"D:\AWORK\wrapper\calculator", "pkg/calculator.py")
+  print(result)
+
+  result = write_file(r"D:\AWORK\wrapper\calculator", "text.txt", "my info")
+  print(result)
+
+  result = write_file(r"D:\AWORK\wrapper\calculator", "pkg2/text.txt" , "my info")
+  print(result)
+
+if __name__ == "__main__":
+    test()
